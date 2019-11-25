28b3588c2b832c7d4677c901f620e5f427948398
PolBorrellas <54673332+PolBorrellas@users.noreply.github.com>
2019-11-25 15:01:00 +0100

    Update code
    app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 server = server.app
 
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
 all_options = {
     'America': ['New York City', 'San Francisco', 'Cincinnati'],
     'Canada': [u'Montréal', 'Toronto', 'Ottawa']
@@ -20,27 +22,26 @@ all_options = {
 
 app.layout = html.Div([
     dcc.RadioItems(
-        id = 'countries-dropdown',
-        options = [{'label': k, 'value': k} for k in all_options.keys()],
-        value = 'America'
+        id='countries-dropdown',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='America'
     ),
 
-    html.Hr(), # Line to separate sections! Designers prefer other methods, such as colored pixels.
+    html.Hr(),  # Line to separate sections! Designers prefer other methods, such as colored pixels.
 
-    dcc.RadioItems(id = 'cities-dropdown'),
+    dcc.RadioItems(id='cities-dropdown'),
 
     html.Hr(),
 
-    html.Div(id = 'display-selected-values')
+    html.Div(id='display-selected-values')
 ])
 
 
-# CALLBACK! When the input changes, the function is called and executed! 
+# CALLBACK! When the input changes, the function is called and executed!
 # 1. Input change + 2. Function executed + 3. Output generated
 @app.callback(
-    dash.dependencies.Output('cities-dropdown', 'options'), 
+    dash.dependencies.Output('cities-dropdown', 'options'),
     [dash.dependencies.Input('countries-dropdown', 'value')])
-
 def set_cities_options(selected_country):
     return [{'label': i, 'value': i} for i in all_options[selected_country]]
 
@@ -49,7 +50,6 @@ def set_cities_options(selected_country):
 @app.callback(
     dash.dependencies.Output('cities-dropdown', 'value'),
     [dash.dependencies.Input('cities-dropdown', 'options')])
-
 def set_cities_value(available_options):
     return available_options[0]['value']
 
@@ -58,12 +58,11 @@ def set_cities_value(available_options):
     dash.dependencies.Output('display-selected-values', 'children'),
     [dash.dependencies.Input('countries-dropdown', 'value'),
      dash.dependencies.Input('cities-dropdown', 'value')])
-
 def set_display_children(selected_country, selected_city):
     return u'{} is a city in {}'.format(
         selected_city, selected_country,
     )
 
+
 if __name__ == '__main__':
     app.run_server()
-
